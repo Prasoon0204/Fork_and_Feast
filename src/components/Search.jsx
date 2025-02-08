46ed0f16e15b4e29be5a58920f25aeed9650f391
Prasoon Thakur <prasoon.thakur0204@gmail.com>
2025-02-08 11:47:13 +0530

    second commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -20,9 +20,9 @@ const Search = () => {
 
   return (
     <div className="p-8 pb-0">
-        <form action={addIngredient} className="flex justify-center gap-4 mb-4 mx-4">
-            <input className="rounded-md max-w-[400px] flex-grow border-2 border-gray-200 px-4" type="text" placeholder="e.g. oregano" name="ingredient" />
-            <button className="rounded-md bg-black text-white py-2 px-8 text-sm">+ Add ingredient</button>
+        <form action={addIngredient} className="sm:flex justify-center gap-4 mb-4 mx-4">
+            <input className="rounded-md w-full sm:max-w-[400px] mb-2 sm:mb-0 flex-grow border-2 border-gray-200 px-4" type="text" placeholder="e.g. oregano" name="ingredient" />
+            <button className="w-full sm:max-w-[180px] rounded-md bg-black text-white py-2 px-8 text-sm">+ Add ingredient</button>
         </form>
         {ingredients.length > 0 && <IngredientsList ingredients = {ingredients} getRecipe={getRecipe} />}
         {recipe !== "" && <ClaudeRecipe recipe = {recipe} />}
